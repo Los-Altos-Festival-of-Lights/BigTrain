659599821ba0d704aff341a799ae3480a6cb667a
Aaron <42322215+aaronllowe@users.noreply.github.com>
2023-08-11 17:30:25 -0700

    update start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 echo "Starting interface..."
-sudo FLASK_APP=interface/app.py flask run -h 0.0.0.0 -p 80
+sudo FLASK_APP=interface/app.py flask run -h 0.0.0.0 -p 80 &
 sleep 5
 firefox http://localhost --kiosk
